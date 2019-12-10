2c0506dcf7f687a2c6a4d37faca1a9c462bfb20c
Mateusz Gdula <gdula.mat@gmail.com>
2019-12-10 23:56:22 +0100

    Create project structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <player-widget msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PlayerWidget from "@/components/PlayerWidget";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    PlayerWidget
   }
 };
 </script>
